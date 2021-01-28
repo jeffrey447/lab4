b741cd446b43ceb41d767a1e1cd60d44e8c496f2
Jeffrey Ha <jha@ucsd.edu>
2021-01-28 00:17:29 -0800

    check

--- routes/hello.js
+++ routes/hello.js
@@ -1,13 +1,9 @@
-
 /*
  * GET home page.
  */
 
-exports.view = function(req, res) {
-  let nameToShow = (req.params.userName || 'World');
-  console.log(`Name is ${nameToShow}.`);
-
-  res.render('hello', {
-  	'name': nameToShow,
+exports.view = function(req, res){
+  res.render('index', {
+  	'name': '',
   });
 };
